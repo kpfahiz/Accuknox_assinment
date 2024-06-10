a04ba3fca4c198308d8d6884f14a896374c0c3bc
fahiz <fahizkppullara@gmail.com>
2024-06-10 07:50:32 +0530

    Login and Signup

---
+++ Social_Networking/user/serializers.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model, authenticate, login, logout
+from rest_framework import serializers
+User = get_user_model()
+
+class SignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        email = data.get('email')
+        password = data.get('password')
+        user = authenticate(email=email, password=password)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError('Invalid credentials')
---
+++ Social_Networking/user/views.py
@@ -0,0 +1,35 @@
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model, authenticate, login, logout
+from rest_framework.views import APIView
+
+from .serializers import SignupSerializer, LoginSerializer
+
+User = get_user_model()
+
+class SignupView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = SignupSerializer
+    permission_classes = [AllowAny]
+
+class LoginView(ObtainAuthToken):
+    serializer_class = LoginSerializer
+    permission_classes = [AllowAny]
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
+class LogoutView(APIView):
+    def post(self, request):
+        request.user.auth_token.delete()
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
